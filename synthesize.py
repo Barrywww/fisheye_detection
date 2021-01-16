be4a1bb5c6a284d0dc157f903a300c4ab549dd60
brilliantTya <fabulous0tya@gmail.com>
2021-01-16 13:48:56 +0800

    commit

--- synthesize.py
+++ synthesize.py
@@ -23,10 +23,10 @@ def crop_center(im: np.ndarray):
 
 def crop_circle(im: np.ndarray):
     im = crop_center(im)
-    alpha = np.zeros((im.shape[0], im.shape[1], 1)).astype(int)
     rr, cc = skimage.draw.disk((OUT_RADIUS, OUT_RADIUS), OUT_RADIUS)
-    alpha[rr, cc] = 1
-    out = np.dstack((im, alpha))
+
+    out = np.zeros(im.shape)
+    out[cc, rr] = im[cc, rr]
     return out
 
 def new_focal_length():
@@ -34,13 +34,25 @@ def new_focal_length():
     fl_px = fl_mm * per_mm
     return fl_mm, fl_px
 
-def fisheye(xy):
+def fisheye_transform(img: np.ndarray):
     f = new_focal_length()[1]
+    uc, vc = np.array(img.shape[:2]) // 2
+
+    xmax = np.array(img.shape[0])
+
+    d = np.sqrt(uc**2 + vc**2)
+    theta0 = np.arctan(d / f)
+    phi0 = np.arctan(vc / uc)
+    r = 2 * theta0 * OUT_RADIUS / np.pi
+    x_scaled= r * np.cos(phi0) + uc
+    return transform.warp(img, _fisheye, map_args={'f': f, 'x_scaled': x_scaled, 'x_max': xmax})
+
+def _fisheye(xy, f, x_scaled, x_max):
     # max_theta = np.arctan(OUT_DIAMETER / f)
     max_theta = np.pi / 2
 
     center = np.mean(xy, axis=0)
-    x, y = (xy - center).T * 2
+    x, y = (xy - center).T * x_max // 2 // x_scaled * 1.5
 
     r = np.sqrt(x**2 + y**2)
     theta = max_theta * r / OUT_RADIUS
@@ -52,9 +64,9 @@ def fisheye(xy):
 
 im = io.imread('data/000000000139.jpg')
 
-# processed_im = crop_circle(im)
-processed_im = im
-out = transform.warp(processed_im, fisheye)
+processed_im = crop_circle(im)
+# processed_im = im
+out = fisheye_transform(processed_im)
 
 f, (ax0, ax1) = plt.subplots(1, 2, subplot_kw=dict(xticks=[], yticks=[]))
 ax0.imshow(processed_im)
